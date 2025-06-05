303919e6aefb16f3232b7ca4c3a3762574be5fd7
Jan Jecmen <jjecmen@azul.com>
2025-06-05 14:58:39 +0000

    Refactor the lifecycle of the server

--- llvm/tools/llvm-lsp/llvm-lsp-server.cpp
+++ llvm/tools/llvm-lsp/llvm-lsp-server.cpp
@@ -8,17 +8,18 @@
 using namespace llvm;
 
 static cl::OptionCategory LlvmLspServerCategory("llvm-lsp-server options");
-static cl::opt<std::string> LogFilePath("log-file", cl::desc("Path to log file"),
+static cl::opt<std::string> LogFilePath("log-file",
+                                        cl::desc("Path to log file"),
                                         cl::init("/tmp/llvm-lsp-server.log"),
                                         cl::cat(LlvmLspServerCategory));
 
 bool LspServer::processRequest() {
   std::string Msg = readMessage();
   LoggerObj.log("Received Message from Client: " + Msg);
+  // TODO: Should we really quit on empty message?
   if (Msg.empty())
     return false;
-  handleMessage(Msg);
-  return true;
+  return handleMessage(Msg);
 }
 
 void LspServer::sendInfo(const std::string &Message) {
@@ -50,15 +51,24 @@ std::string LspServer::readMessage() {
   return JsonStr;
 }
 
-void LspServer::sendResponse(const json::Value &ID,
-                             const json::Value &Response) {
-  json::Object ResponseObj{
-      {"jsonrpc", "2.0"}, {"id", ID}, {"result", Response}};
+void LspServer::sendMessage(const json::Value &ID, const std::string &Kind,
+                            const json::Value &Payload) {
+  json::Object ResponseObj{{"jsonrpc", "2.0"}, {"id", ID}, {Kind, Payload}};
   std::string Output;
   raw_string_ostream OutStr(Output);
   OutStr << json::Value(std::move(ResponseObj));
   std::cout << "Content-Length: " << Output.size() << "\r\n\r\n" << Output;
   std::cout.flush();
+}
+
+void LspServer::sendResponse(const json::Value &ID,
+                             const json::Value &Response) {
+  sendMessage(ID, "result", Response);
+}
+
+void LspServer::sendErrorResponse(const llvm::json::Value &ID, const int Code,
+                                  const std::string &Message) {
+  sendMessage(ID, "error", json::Object{{"code", Code}, {"message", Message}});
 }
 
 void LspServer::sendNotification(const std::string &RPCMethod,
@@ -111,7 +121,10 @@ void LspServer::handleRequestInitialize(const json::Value *Id,
               {"openClose", true},
               {"change", 0}, // We dont want to sync the documents.
           }
-        }
+        },
+        {"referencesProvider", true},
+        {"hoverProvider", true},
+        {"codeActionProvider", true},
       }
     }
   };
@@ -209,40 +222,113 @@ void LspServer::handleRequestGetBBLocation(const json::Value *Id,
   sendResponse(*Id, json::Value(std::move(ResponseParams)));
 }
 
-void LspServer::handleMessage(const std::string &JsonStr) {
+bool LspServer::handleMessage(const std::string &JsonStr) {
   auto Val = json::parse(JsonStr);
   assert(Val && "Error Parsing JSON String!");
 
   const json::Object *Obj = Val->getAsObject();
   assert(Obj && "Expected valid JSON Object!");
 
-  std::string Method = Obj->getString("method")->str();
-
-  const json::Value *Params = Obj->get("params");
-  assert(Params && "Expected valid Params field!");
-
+  const std::string Method = Obj->getString("method")->str();
   const json::Value *Id = Obj->get("id");
+  const json::Value *Params = Obj->get("params");
 
-  if (Method == "initialize") {
-    assert(Id && "Expected valid ID field!");
-    handleRequestInitialize(Id, Params);
-  } else if (Method == "initialized") {
-    // ignore
-  } else if (Method == "textDocument/didOpen") {
-    handleNotificationTextDocumentDidOpen(Id, Params);
-  } else if (Method == "llvm/getCfg") {
-    handleRequestCFGGen(Id, Params);
-  } else if (Method == "llvm/cfgNode") {
-    sendInfo("Reminder: llvm/cfgNode is work in progress, sending dummy data!");
-    handleRequestGetCFGNode(Id, Params);
-  } else if (Method == "llvm/bbLocation") {
-    sendInfo(
-        "Reminder: llvm/bbLocation is work in progress, sending dummy data!");
-    handleRequestGetBBLocation(Id, Params);
-  } else {
+  // TODO: some methods can only be sent once, enforce it
+  //   these include: initialize, initialized, shutdown, exit
+  switch (State) {
+
+  case LspServerState::Starting: {
+    if (Method == "initialize") {
+      assert(Id && "Expected valid ID field!");
+      assert(Params && "Expected valid Params field!");
+      switchToState(LspServerState::Initializing);
+      handleRequestInitialize(Id, Params);
+      return true;
+    }
+    // For requests, reply with error code
+    if (Id) {
+      sendErrorResponse(*Id, LspErrorCode::RequestDuringInitialization, "");
+      return true;
+    }
+    // For notifications, only process 'exit'
+    if (Method == "exit") {
+      switchToState(LspServerState::Exitted);
+      return false;
+    }
+    // Ignore the rest
+    return true;
+  }
+
+  case LspServerState::Initializing: {
+    if (Method == "initialized") {
+      switchToState(LspServerState::Ready);
+      return true;
+    }
+    // TODO: shouldn't be getting anything, for now just ignore it if we do
+    return true;
+  }
+
+  case LspServerState::Ready: {
+    if (Method == "shutdown") {
+      switchToState(LspServerState::ShuttingDown);
+      sendErrorResponse(*Id, LspErrorCode::InvalidRequest, "");
+      return true;
+    }
+
+    // Ignored for now
+    if (Method == "textDocument/hover")
+      return true;
+    if (Method == "$/cancelRequest")
+      return true;
+
+    if (Method == "textDocument/didOpen") {
+      handleNotificationTextDocumentDidOpen(Id, Params);
+      return true;
+    }
+    if (Method == "textDocument/references") {
+      // TODO
+      return true;
+    }
+    if (Method == "llvm/getCfg") {
+      handleRequestCFGGen(Id, Params);
+      return true;
+    }
+    if (Method == "llvm/cfgNode") {
+      sendInfo(
+          "Reminder: llvm/cfgNode is work in progress, sending dummy data!");
+      handleRequestGetCFGNode(Id, Params);
+      return true;
+    }
+    if (Method == "llvm/bbLocation") {
+      sendInfo(
+          "Reminder: llvm/bbLocation is work in progress, sending dummy data!");
+      handleRequestGetBBLocation(Id, Params);
+      return true;
+    }
     // TODO: handle other LSP methods
     sendInfo("[WIP] Unhandled RPC call : " + Method);
+    return true;
   }
+
+  case LspServerState::ShuttingDown: {
+    if (Method == "exit") {
+      switchToState(LspServerState::Exitted);
+      // TODO: not sure what's wrong but restarting the lsp from vscode never
+      // gets here... figure out why
+      LoggerObj.log("Bye!");
+      return false;
+    }
+    // TODO: shouldn't be getting anything, for now just quit if we do
+    return false;
+  }
+
+  case LspServerState::Exitted: {
+    // TODO: shouldn't be getting anything, for now just quit if we do
+    return false;
+  }
+  }
+  // silence warning
+  return true;
 }
 
 int main(int argc, char **argv) {
@@ -253,5 +339,6 @@ int main(int argc, char **argv) {
 
   while (LS.processRequest())
     ;
-  return 0;
+
+  return LS.getExitCode();
 }
--- llvm/tools/llvm-lsp/llvm-lsp-server.h
+++ llvm/tools/llvm-lsp/llvm-lsp-server.h
@@ -1,6 +1,8 @@
 #ifndef LLVM_LSP_SERVER_H
 #define LLVM_LSP_SERVER_H
 
+#include <sstream>
+
 #include "llvm/Support/JSON.h"
 
 #include "Logger.h"
@@ -8,11 +10,52 @@
 class LspServer {
   Logger LoggerObj;
 
+  enum class LspServerState {
+    Starting,
+    Initializing,
+    Ready,
+    ShuttingDown, // Received 'shutdown' message
+    Exitted,      // Received 'exit' message
+  } State = LspServerState::Starting;
+
+  std::string stateToString(LspServerState S) {
+    switch (S) {
+    case LspServerState::Starting:
+      return "Starting";
+    case LspServerState::Initializing:
+      return "Initializing";
+    case LspServerState::Ready:
+      return "Ready";
+    case LspServerState::ShuttingDown:
+      return "ShuttingDown";
+    case LspServerState::Exitted:
+      return "Exitted";
+    }
+    return "<UNKNOWN STATE>";
+  }
+
+  void switchToState(LspServerState NewState) {
+    std::stringstream SS;
+    SS << "Changing State from " << stateToString(State) << " to "
+       << stateToString(NewState);
+    LoggerObj.log(SS.str());
+    State = NewState;
+  }
+
+  enum LspErrorCode {
+    RequestDuringInitialization = -32002,
+    ParseError = -32700,
+    InvalidRequest = -32600,
+    MethodNotFound = -32601,
+    InvalidParams = -32602,
+    InternalError = -32603,
+  };
+
   llvm::DenseMap<llvm::StringRef, std::string> SVGToIRMap;
 
 public:
   LspServer(const std::string Logfile) : LoggerObj(Logfile) {
-    LoggerObj.log("Starting LLVM LSP Server\n");
+    LoggerObj.log("Starting LLVM LSP Server");
   }
 
   // Receives one message via stdin and responds to it.
@@ -21,13 +64,22 @@ public:
   // Sends a message to client as INFO notification
   void sendInfo(const std::string &Message);
 
+  // The process exit code, should be success only if the State is Exitted
+  int getExitCode() { return State == LspServerState::Exitted ? 0 : 1; }
+
 private:
   // Returns the JSON String encoded in the message
   std::string readMessage();
 
+  // Send message (response with either success or error)
+  void sendMessage(const llvm::json::Value &ID, const std::string &Kind,
+                   const llvm::json::Value &Payload);
+
   // Given a Response message as JSON value, send it over stdout.
   void sendResponse(const llvm::json::Value &ID,
                     const llvm::json::Value &Response);
+  void sendErrorResponse(const llvm::json::Value &ID, const int Code,
+                         const std::string &Message);
 
   // Given a Notification message as JSON value, send it over stdout.
   void sendNotification(const std::string &RPCMethod,
@@ -83,7 +135,7 @@ private:
                                   const llvm::json::Value *Params);
 
   // Identifies RPC Call and dispatches the handling to other methods
-  void handleMessage(const std::string &JsonStr);
+  bool handleMessage(const std::string &JsonStr);
 };
 
 #endif // LLVM_LSP_SERVER_H
